6f60abe68bdd85dcff29e51cff69913f89c45f64
Esteban Pisani <estebandpisani@gmail.com>
2024-09-25 21:00:01 -0300

    Ej 1 y 3 con memoria dinámica.

--- practica1-1.c
+++ practica1-1.c
@@ -1,12 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+#define ROWS 3
+#define COLUMNS 3
 
-	int matriz[5][5];
+int main()
+{
+
+	int rows = ROWS;
+	int columns = COLUMNS;
+
+	// Versión con arreglos (Memoria STACK)
+	/*
+	int matriz[rows][columns];
 
 	for(int i=0; i<5; i++){
 		for(int j=0; j<5; j++){
-		
+
 			if(i==j){
 				matriz[i][j]= 1;
 			} else {
@@ -16,6 +26,44 @@ int main(){
 		}
 		printf("\n");
 	}
+	*/
+
+	/////////Versión en memoria dinámica HEAP////////////////////
+
+	// Reservo espacio para cada FILA
+	int **matrizHeap = (int **)malloc(rows * sizeof(int *));
+	// Inicializo el primer espacio con la cantidad total de memoria que necesito
+	matrizHeap[0] = (int *)malloc(rows * columns * sizeof(int));
+	// Inicializo para cada fila una porción del espacio reservado en el primero.
+	// De esta manera quedan reservados espacios contiguos en la memoria
+	for (int i = 1; i < rows; i++)
+	{
+		matrizHeap[i] = matrizHeap[0] + columns * i; // Nomenclatura de arreglos
+		printf("matrizHeap[%d] = %p\n", i, matrizHeap[i]);
+	}
+	// Inicializo cada columna
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			if (i == j)
+			{
+				*(matrizHeap[i] + j) = 1; // Nomenclatura de arreglos
+			}
+			else
+			{
+				*((matrizHeap[i]) + j) = 0;
+			}
+			printf("%d ", *((matrizHeap[i]) + j));
+			if(j == columns-1){
+				printf("\n");
+			}
+		}
+	}
+	// Liberar memoria HEAP (una por cada malloc)
+
+	free(matrizHeap[0]);
+	free(matrizHeap);
 
 	return 0;
 }
--- practica1-3.c
+++ practica1-3.c
@@ -2,13 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define ROWS 3
+#define COLUMNS 3
+
 int main()
 {
-
-    int matrizA[5][5];
-    int matrizB[5][5];
-
     srand(time(NULL));
+
+    int rows = ROWS;
+    int columns = COLUMNS;
+
+    // Memoria STACK
+    /*
+    int matrizA[rows][columns];
+    int matrizB[rows][columns];
 
     for (int i = 0; i < 5; i++)
     {
@@ -19,7 +26,7 @@ int main()
         }
     }
 
-    printf("\nMatriz A: \n");
+        printf("\nMatriz A: \n");
 
     for (int i = 0; i < 5; i++)
     {
@@ -48,6 +55,69 @@ int main()
         for (int j = 0; j < 5; j++)
         {
             printf("%d ", matrizA[i][j] + matrizB[i][j]);
+        }
+        printf("\n");
+    }
+*/
+
+    // Memoria HEAP
+
+    int **matrizA = (int **)malloc(sizeof(int *) * rows);
+    matrizA[0] = (int *)malloc(sizeof(int) * columns * rows);
+
+    int **matrizB = (int **)malloc(sizeof(int *) * rows);
+    matrizB[0] = (int *)malloc(sizeof(int) * columns * rows);
+
+    for (int i = 1; i < rows; i++)
+    {
+        matrizA[i] = matrizA[0] + columns * i;
+        matrizB[i] = matrizB[0] + columns * i;
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            *(matrizA[i] + j) = (rand() % 11);
+            *(matrizB[i] + j) = (rand() % 11);
+        }
+    }
+
+    printf("\nMatriz A: \n");
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            printf("%d ", *(matrizA[i] + j));
+            if (j == columns - 1)
+            {
+                printf("\n");
+            }
+        }
+    }
+
+    printf("\nMatriz B: \n");
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            printf("%d ", *(matrizB[i] + j));
+            if (j == columns - 1)
+            {
+                printf("\n");
+            }
+        }
+    }
+
+    printf("\nSuma matrices A y B: \n");
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            printf("%d ", *(matrizA[i] + j) + *(matrizB[i] + j));
         }
         printf("\n");
     }
